bef71bacc032f97979ce80be69caab6aec407896
Chris Aslanoglou <chris.aslanoglou@gmail.com>
2017-06-28 15:26:12 +0200

    [WIP] terminal symbols
    Signed-off-by: Chris Aslanoglou <chris.aslanoglou@gmail.com>

--- src/inspire_query_parser/parser.py
+++ src/inspire_query_parser/parser.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, print_function
-from pypeg2 import attr, Keyword, Literal, parse, omit, optional, re, Symbol, word, K, Enum
+from pypeg2 import attr, Keyword, Literal, parse, omit, optional, re, Symbol, word, K, Enum, contiguous, some, \
+    maybe_some
 
 import ast
 from config import INSPIRE_PARSER_KEYWORDS
@@ -29,8 +30,12 @@ class ListRule(ast.ListOp):
 # #### Keywords ####
 class Find(Keyword):
     keyword = "find"
-    regex = re.compile(r"({0})\s".format("|".join([keyword[:i] for i in range(len(keyword) + 1, 0, -1)])), re.IGNORECASE)
-    grammar = Enum(*[k for i in range(1, len(keyword) + 1) for k in (K(keyword[:i] + " "), K(keyword[:i].upper() + " "))])
+    regex = re.compile(
+        r"({0})\s".format("|".join([keyword[:i] for i in range(len(keyword) + 1, 0, -1)])),
+        re.IGNORECASE)
+    grammar = Enum(
+        K("find"), K("FIND"),
+        *[k for i in range(1, len(keyword) + 1) for k in (K(keyword[:i] + " "), K(keyword[:i].upper() + " "))])
 
 
 class Fulltext(Keyword):
@@ -68,9 +73,18 @@ class Qualifier(LeafRule):
     grammar = attr('value', re.compile(r"({0})\b".format("|".join(INSPIRE_PARSER_KEYWORDS))))
 
 
-class NormalPhrase(LeafRule):
+class Terminal(LeafRule):
     Symbol.check_keywords = True
-    grammar = attr('value', Symbol)
+    Symbol.regex = re.compile(r"(\w+([-/]\w+)*)")
+    grammar = attr('value', Symbol), maybe_some([" ", ",", "."])
+
+
+class Terminals(ListRule):
+    grammar = contiguous(attr('children', some(Terminal)))
+
+
+class NormalPhrase(UnaryRule):
+    grammar = attr('op', Terminals)
 
 
 class NormalPhraseSpanTail(LeafRule):
@@ -186,45 +200,53 @@ QueryExpressionTail.grammar = [
 
 class StartRule(UnaryRule):
     grammar = [
+        attr('op', QueryExpression),
         (omit(Find), attr('op', QueryExpression)),
-        attr('op', QueryExpression)
     ]
 # ########################
 
 
 if __name__ == '__main__':
     # Find keyword combined with other production rules
-    print(parse('find author "ellis"', StartRule))
-    print(parse("FIN author:'ellis'", StartRule))
-    print(parse('f author ellis', StartRule))
+    # print(parse('find author "ellis"', StartRule))
+    # print(parse("FIN author:'ellis'", StartRule))
+    # print(parse('f author ellis', StartRule))
 
     # Invenio like search
-    print(parse("author:ellis", StartRule))
+    # print(parse("author:ellis", StartRule))
 
     # Boolean
-    print(parse("author ellis and title 'boson'", StartRule))
-    print(parse("author ellis AND title boson", StartRule))
-    print(parse("author ellis | title 'boson'", StartRule))
-    print(parse("author ellis OR title 'boson'", StartRule))
-    print(parse("author ellis + title 'boson'", StartRule))
-    print(parse("author ellis & title 'boson'", StartRule))
+    # print(parse("author ellis and title 'boson'", StartRule))
+    # print(parse("author ellis AND title boson", StartRule))
+    # print(parse("author ellis | title 'boson'", StartRule))
+    # print(parse("author ellis OR title 'boson'", StartRule))
+    # print(parse("author ellis + title 'boson'", StartRule))
+    # print(parse("author ellis & title 'boson'", StartRule))
 
     # Negation
-    print(parse("author ellis and not title 'boson'", StartRule))
-    print(parse("author ellis and not title boson", StartRule))
-    print(parse("-title 'boson'", StartRule))
+    # print(parse("author ellis and not title 'boson'", StartRule))
+    # print(parse("author ellis and not title boson", StartRule))
+    # print(parse("-title 'boson'", StartRule))
 
     # Nested expressions
-    print(parse("author ellis and (title boson or (author /^xi$/ and title foo))", StartRule))
+    # print(parse("author ellis and (title boson or (author /^xi$/ and title foo))", StartRule))
 
     # Metadata search
-    print(parse("fulltext:boson", StartRule))
-    print(parse("reference ellis", StartRule))
-    print(parse('reference "Ellis"', StartRule))
+    # print(parse("fulltext:boson", StartRule))
+    # print(parse("reference ellis", StartRule))
+    # print(parse('reference "Ellis"', StartRule))
 
     # Only phrases
-    print(parse('ellis', StartRule))
-    print(parse("'ellis'", StartRule))
+    # print(parse('ellis', StartRule))
+    # print(parse("'ellis'", StartRule))
 
+    # Non trivial terminals
+    # print(parse("find Higgs boson", StartRule))
+    # print(parse("author ellis, j.", StartRule))
+    # print(parse("author j., ellis", StartRule))
+    # print(parse("f title Super Collider Physics", StartRule))
+    # print(parse("find title Alternative the Phase-II upgrade of the ATLAS Inner Detector", StartRule))
+    # print(parse("find title na61/shine", StartRule))
+    print(parse("title foo and author abtrall", StartRule))
     # print(parse("title e-10", StartRule))
     # print(parse("title e,10 and author ellis", StartRule))
